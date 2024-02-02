80b09efb9079cb2731f2c2a7b38c79393570e988
QuintinoPro <lucasquintinotrade@gmail.com>
2024-02-02 19:30:12 -0300

    Estudo 24-02-02

---
+++ Javascript/aula02.js
@@ -0,0 +1,28 @@
+
+
+// Estrutura Condicional 
+
+const camisetaAlunoAzul = true;
+const camisetaProfAzul = false;
+
+
+const num = 0;
+
+const NumeroPar =  num % 2  === 0;
+
+console.log(NumeroPar);
+
+
+if(num === 0) {
+    console.log("Numero Invalido!");
+}
+else if(num === 5){
+    console.log("Numero Invalido!");
+}
+else if(NumeroPar){
+    console.log("Par");
+}
+else{
+    console.log("Impar!");
+}
+
--- Javascript/desafio02.js
+++ Javascript/desafio02.js
@@ -1,3 +1,5 @@
+
+
 const precoEtanol = 5;
 const precoGasolina = 7;
 
@@ -6,7 +8,7 @@ const tipoCombstCarro = 'Etanol';
 const kmPorLitro = 10;
 const distanciaKm = 100;
 const litrosfinal = distanciaKm / kmPorLitro;
-const valorfinal = 0;
+let valorfinal = 0;
 
 
 if (tipoCombstCarro === 'Etanol'){
@@ -18,3 +20,4 @@ if (tipoCombstCarro === 'Etanol'){
     valorfinal = litrosfinal * precoGasolina;
     console.log("Valor que sera gasto: ", valorfinal);
 }
+
